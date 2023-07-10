90e74660417f4a903969eae443cc514860324a18
brewchetta <chett.tiller@gmail.com>
2023-07-10 12:53:30 -0400

    added a practice section

---
+++ practice.js
@@ -0,0 +1,20 @@
+// PRACTICE //
+
+// // // // // 
+// given an array 
+//  const elements = ["hydrogen", "helium", "lithium", "beryllium", "boron", "carbon", "nitrogen", "oxygen"] 
+// can you write a function that `console.log`s each element like so: "hydrogen is an element", "helium is an element"... etc
+// can you write a function that returns a NEW ARRAY for each element like so: "hydrogen is an element", "helium is an element" 
+// BONUS: can you write a function that returns a NEW ARRAY for each element like so: "hydrogen is element number 1", "helium is element number 2" 
+// can you write a function to find THE FIRST word that starts with the letter "b"?
+// can you write a function to find EVERY word that starts with the letter "b"?
+// // // // // 
+
+// // // // // 
+// given an object 
+//  const chett = { name: "Chett", cats: ["Ursula", "Octavia", "Luna"] }
+// can you console.log the name inside the object?
+// can you console.log the numberOfCats in the object like so: "{name} has {number of cats} cats"
+// can you amend the console.log above so it reads: "{name} has {number of cats} cats named {cat name}, {cat name}, {cat name}"
+// BONUS: for the cat names above, can you make it so it dynamically iterates through the cat names instead of hard coding them? if you're stuck then look up `.join`
+// // // // // 
