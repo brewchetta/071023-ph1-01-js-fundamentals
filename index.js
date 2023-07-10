2f328e67b327dbd189ff38ce02856d2968352695
brewchetta <chett.tiller@gmail.com>
2023-07-10 17:59:27 -0400

    lecture 1 lots of info huzzah

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('hello world')
+// console.log('hello world')
+
+// WHAT IS A FUNCTION???
+// an easy way to perform the same task over and over without rewriting
+
+function printName(name) {
+    console.log(`My name is ${name}`)
+}
+
+const someOtherFunction = function() {
+    console.log("I am declared with const")
+}
+
+const myArrowFunc = (name) => {
+    console.log("I am an arrow function " + name)
+}
+
+// function add(num1, num2) {
+    //     return num1 + num2
+    // }
+    
+    const add = (num1, num2) => num1 + num2
+    // the numbers above will IMPLICITLY RETURN since this is an arrow function
+    
+    // const add = (num1, num2) => console.log(num1 + num2)
+    
+    if (0) {
+        console.log("Hello I am true")
+    } else {
+        console.log("Goodbye I am false")
+    }
+    
+    // 0 is falsy so this will go to the `else` statement
+    
+const animals = ["moose", "raccoon", "chicken", "crab", "ferret"]
+
+// function sayAnimalName(animal) {
+//     if (animal === "raccoon") {
+//         console.log(`${animal} is a great animal`)
+//     } else {
+//         console.log(`${animal} is just ok`)
+//     }
+// }
+
+animals.forEach( animal => console.log(animal) )
--- practice.js
+++ practice.js
@@ -8,15 +8,44 @@ const elementsArr = ["hydrogen", "helium", "lithium", "beryllium", "boron", "car
 
 // can you write a function that `console.log`s each element like so: "hydrogen is an element", "helium is an element"... etc
 
+function elementLogger(arr) {
+    arr.forEach( element => console.log(`${element} is an element`) )
+}
+
 // can you write a function that returns a NEW ARRAY for each element like so: "hydrogen is an element", "helium is an element" 
 
-// BONUS: can you write a function that returns a NEW ARRAY for each element like so: "hydrogen is element number 1", "helium is element number 2" 
+function makeNewArray(elementsArr) {
+    return elementsArr.map( element => `${element} is an element` )
+}
+
+// BONUS: can you write a function that returns a NEW ARRAY with each element like so: "hydrogen is element number 1", "helium is element number 2" 
+
+function orderedElementArrayBuilder(array) {
+    // const result = []
+    // for (let i = 0; i < array.length; i++) {
+    //         result.push(`${array[i]} is element number ${i}`)
+    //     }
+    //     return result
+    return elementsArr.map( (element, index) => `${element} is element ${index + 1}` )
+}
 
 // can you write a function to find THE FIRST word that starts with the letter "b"?
+
+function firstB(array) {
+    // for (const element of array) {
+    //     if (element.startsWith("b")) {
+    //         return element
+    //     }
+    // }
+    return array.find( element => element.startsWith("b") )
+}
+
 // can you write a function to find EVERY word that starts with the letter "b"?
+
+function everyB(array) {
+    return array.filter( element => element.startsWith("b") )
+}
 // // // // // //
-
-
 
 // // // // // //
 // given an object 
